9c780dea34797d3ae4dbebd38e9e1d60665b6eaa
Migoo <migoo@MigiBook.local>
2008-12-16 16:42:24 +0100

    added validations

--- app/models/locale.rb
+++ app/models/locale.rb
@@ -1,4 +1,6 @@
 class Locale < ActiveRecord::Base
   has_many :translations
   has_many :keys, :through => :translations
+  
+  validates_presence_of :name, :iso_code
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,6 +1,7 @@
 class Translation < ActiveRecord::Base
   belongs_to :locale
   belongs_to :key
+  validates_presence_of :value, :locale_id, :key_id
   
   def self.find_keys_by_locale(locale_id)
     key = self.find(:all, :select => [ :key_id ], :conditions => { :locale_id => locale_id })
