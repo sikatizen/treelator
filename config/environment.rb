a694322d37ce31a948ee0e0f35d937d7cee1e95c
Noé Froidevaux <noe.froidevaux@gmail.com>
2009-03-04 15:25:00 +0100

    improve sikalator design

--- config/environment.rb
+++ config/environment.rb
@@ -73,4 +73,7 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 end
+
+# Sass::Plugin.options[:style] = :compact
+
 LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/"
